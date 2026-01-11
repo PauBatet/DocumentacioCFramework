07c85535bae3bf85efb05c96200280c8f006395c
Pau Batet <paubatet@hotmail.com>
2026-01-11 05:40:26 +0100

    Implementació pàgina documentació

--- routes.c
+++ routes.c
@@ -2,4 +2,11 @@
 
 Route routes[] = {
 	{"/", home},
+	{"/docs", docs},
+	{"/routing", routes_view},
+	{"/views", views},
+	{"/models", models},
+	{"/templates", templates},
+	{"/docker", docker},
+	{"/core-concepts", core_concepts},
 };
--- views.c
+++ views.c
@@ -10,3 +10,38 @@ void home(HTTPRequest *request, Database *db) {
     (void)db;
     render_html(request, "home.html", NULL, 0);
 }
+
+void docs(HTTPRequest *request, Database *db) {
+    (void)db;
+    render_html(request, "docs.html", NULL, 0);
+}
+
+void routes_view(HTTPRequest *request, Database *db) {
+    (void)db;
+    render_html(request, "routing.html", NULL, 0);
+}
+
+void views(HTTPRequest *request, Database *db) {
+    (void)db;
+    render_html(request, "views.html", NULL, 0);
+}
+
+void models(HTTPRequest *request, Database *db) {
+    (void)db;
+    render_html(request, "models.html", NULL, 0);
+}
+
+void templates(HTTPRequest *request, Database *db) {
+    (void)db;
+    render_html(request, "templates.html", NULL, 0);
+}
+
+void docker(HTTPRequest *request, Database *db) {
+    (void)db;
+    render_html(request, "docker.html", NULL, 0);
+}
+
+void core_concepts(HTTPRequest *request, Database *db) {
+    (void)db;
+    render_html(request, "core_concepts.html", NULL, 0);
+}
